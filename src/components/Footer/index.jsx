4e01687af6e79d5d5ed48a5d23733b41d40410dd
OleksSkoryk <olekscraft@gmail.com>
2025-06-27 23:29:20 +0300

    Fully finished main page and Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,20 @@
 import './style.css';
 
+import { useTranslation } from 'react-i18next';
+
+import Logo from 'components/Logo';
+import logo2 from 'resources/logo2.png';
+
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
     <div className="Footer">
-      
+      <p className='footer-text'>© 2025 Dixie. All rights reserved. | Dixie™ and the Dixie logo are trademarks of Pegasus Tecnologies. All product names, logos, and brands are property of their respective owners. Features, pricing, and availability are subject to change without notice.</p>
+      <div className='footer-logos'>
+        <Logo />
+        <img src={logo2} alt="Pegasus Tecnologies Logo" className='pegasus-logo' />
+      </div>
     </div>
   );
 }
--- src/pages/Download/index.jsx
+++ src/pages/Download/index.jsx
@@ -1,11 +1,13 @@
 import './style.css';
 import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 export default function Download() {
   return (
     <div className="App">
       <Header />
       <h1>Download Dixie Assistant App</h1>
+      <Footer />
     </div>
   );
 }
